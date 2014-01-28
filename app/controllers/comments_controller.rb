62166da3e480cc48920dab498fb4b566a3f120c4
Tyler Dimon <tyler@nextupdate.com>
2014-01-28 17:27:48 -0600

    Finish Tealeaf week 3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
+  before_action :require_user
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.user = User.first # TODO: change once we have authentication
+    @comment.user = current_user
     
     if @comment.save
       flash[:notice] = "Your comment was added."
@@ -10,5 +12,17 @@ class CommentsController < ApplicationController
     else
       render 'posts/show'
     end
+  end
+  
+  def vote
+    comment = Comment.find(params[:id])
+    vote = Vote.create(voteable: comment, user: current_user, vote: params[:vote])
+    
+    if vote.valid?
+      flash[:notice] = "Your vote was counted."
+    else
+      flash[:error] = "You can only vote on a comment once."
+    end
+    redirect_to :back
   end
 end
